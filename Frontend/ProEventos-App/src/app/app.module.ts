ba8536a2a0369e07257f1a5bdb360a843380bc3d
Rubens Rudio <rubensrudio@gmail.com>
2022-02-24 01:23:04 -0300

    Iniciando Grid de eventos

--- Frontend/ProEventos-App/src/app/app.module.ts
+++ Frontend/ProEventos-App/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http'
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-      PalestrantesComponent
+      PalestrantesComponent,
+      NavComponent
    ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
     AppRoutingModule,
     HttpClientModule
   ],
